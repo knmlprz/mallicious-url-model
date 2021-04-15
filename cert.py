fed8d736152a49b5cfc2b5be05539859922aa0f8
finloop <piotr.krawiec23@gmail.com>
2021-04-15 22:24:15 +0200

    Download data from cert.pl
    Downloaded data is saved in file as buffer which can be refreshed with `force_download=True`.

---
+++ cert.py
@@ -0,0 +1,37 @@
+import requests
+import os.path
+
+URL = 'https://hole.cert.pl/domains/domains.json'
+FILENAME = 'cert.json'
+
+
+def get_data(url=URL, force_download=False, filename=FILENAME):
+    """
+    Download data from url save it to file as text.
+
+    Parameters
+    ----------
+    url : str
+        Url to dataset.
+    force_download : bool
+        If True data is downloaded from url, otherwise loaded from file
+    filename : str
+        Name of file to load/save
+
+    Returns
+    -------
+    str
+        Text response (json format)
+    """
+
+    if force_download or not os.path.exists(filename):
+        # Download file and save it
+        text_response = requests.get(url).text
+        with open(filename, 'w') as f:
+            f.write(text_response)
+    else:
+        # Check if file exists, load if so
+        with open(filename, 'r') as f:
+            text_response = f.read()
+
+    return text_response
