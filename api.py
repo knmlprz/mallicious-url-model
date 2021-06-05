92ef37e1df2acb1459017ea141e50cb6f98b1e10
Patryk Gronkiewicz <patryk@gronkiewicz.dev>
2021-06-05 14:48:43 +0200

    create api applying our model

--- api.py
+++ api.py
@@ -1,10 +1,13 @@
 from flask import Flask, request
 from random import random
+from model import Model
+import json
 
 app = Flask(__name__)
+model = Model()
 
 
 @app.route("/", methods=["POST"])
 def main():
-
-    return {"sus": "good" if round(random()) == 1 else "bad"}
+    domain = request.get_json(force=True)["domain"]
+    return {"sus": model.predict(domain)}
